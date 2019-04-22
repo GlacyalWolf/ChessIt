eebf7f4518b846d7e53add44a73606a0dcb2369d
Isvy Araujo <ikelisonva.jmzafra@gmail.com>
2019-04-22 15:50:38 +0200

    class jugador first update

---
+++ src/Jugador.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Jugador{
+    private static BufferedReader reader;
+    static String correo;
+    static Boolean color;
+
+    Jugador(String correo,Boolean color){
+        this.correo = correo;
+        this.color = color;
+        if (color == false){
+            System.out.println(correo);
+            System.out.print("Tu color asginado es el negro.");
+        } else {
+            System.out.println(correo);
+            System.out.print("Tu color asginado es el blanco.");
+        }
+
+    }
+
+}
--- src/Torre.java
+++ src/Torre.java
@@ -1,12 +1,32 @@
-/*public class Torre{
-    private int x;
-    private int y;
-    private boolean color;
+/*
+public class Torre extends Ficha {
+int y1=this.y;
+int x1=this.x;
 
 
-    static int[] movimiento{
+while(y1>0){
+    y1=y1-1;
+    System.out.print(x+","+y1);
 
     }
-}
- */
+    y1=this.y;
+while (y1<7){
+    y1=y1+1;
+    System.out.print(x,y1);
 
+
+    }
+    x1=x;
+while (x1>0)
+
+    {
+    x1=x1-1;
+    print(x1,y);
+    x1=x;
+    }
+while (x1<7){
+    x1 = x1 + 1;
+    print(x1, y);
+    }
+}
+*/
