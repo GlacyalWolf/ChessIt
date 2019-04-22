89462a57d4ac6a7676e05b92f4e9fdfac51a7d2f
GlacyalWolf <ismaelromero1999@gmail.com>
2019-04-22 15:53:14 +0200

    setrs i geters de torre i caballo

--- src/Torre.java
+++ src/Torre.java
@@ -3,6 +3,23 @@ public class Torre{
     private int y;
     private boolean color;
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
     static int posibilidades(int x,int y){
         int y1=y;
         int x1=x;
